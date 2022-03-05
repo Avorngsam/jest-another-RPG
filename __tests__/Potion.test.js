709c28c83086ab88f62f9e2d2c8a8cd884c6f4ab
Angelina <atvorngsam@gmail.com>
2022-03-04 20:19:16 -0600

    edits

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,21 +1,5 @@
 const Potion = require('../lib/Potion.js');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-test('creates a health potion object', () => {
-    const potion = new Potion('health');
-  
-    expect(potion.name).toBe('health');
-=======
-test('creates a random potion object', () => {
-    const potion = new Potion();
-  
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
->>>>>>> feature/potion
-    expect(potion.value).toEqual(expect.any(Number));
-  });
-=======
 test('creates a health potion object', () => {
   const potion = new Potion('health');
 
@@ -30,4 +14,3 @@ test('creates a random potion object', () => {
   expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });
->>>>>>> feature/potion
