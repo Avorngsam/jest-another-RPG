84b603defaab57f7125508cc0eb9f22986f65afd
Angelina <atvorngsam@gmail.com>
2022-03-04 21:36:06 -0600

    Added mocks

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,20 +1,5 @@
 const Potion = require('../lib/Potion.js');
 
-
-test('creates a health potion object', () => {
-    const potion = new Potion('health');
-  
-    expect(potion.name).toBe('health');
-
-test('creates a random potion object', () => {
-    const potion = new Potion();
-  
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-
-    expect(potion.value).toEqual(expect.any(Number));
-  });
-
 test('creates a health potion object', () => {
   const potion = new Potion('health');
 
